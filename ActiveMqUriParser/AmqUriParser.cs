7175d65c3d2456a7954d0131f3f2aef1b4be9d8b
Mikhail Kalatchev <kalatchev@gmail.com>
2024-05-08 13:41:27 +0300

    Add project files.

---
+++ ActiveMqUriParser/AmqUriParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Ignore Spelling: amq
+
+namespace Skyware.ActiveMq;
+
+public class AmqUriParser : GenericUriParser
+{
+    public AmqUriParser(GenericUriParserOptions options) : base(options)
+    {
+    }
+
+    // TODO: See https://www.codeproject.com/Articles/13773/Writing-a-custom-UriParser-for-NET
+
+    // ActiveMQ Docs: https://activemq.apache.org/components/classic/documentation/connection-configuration-uri 
+}
